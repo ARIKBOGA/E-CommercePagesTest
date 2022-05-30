d08aac1669f08dc21cd98c10aad1ebee36a68d2c
burakarikboga <barikboga42@gmail.com>
2022-05-30 19:04:12 +0300

    TestBase.java class has been created to use in upcoming tasks by extending

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,24 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
