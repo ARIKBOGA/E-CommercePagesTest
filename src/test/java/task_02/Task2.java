0c43703a98be22967a147f98fd931fa77ad2c97f
burakarikboga <barikboga42@gmail.com>
2022-05-31 19:29:56 +0300

    MoneyGaming web-page UI test completed

--- src/test/java/task_02/Task2.java
+++ src/test/java/task_02/Task2.java
@@ -1,6 +1,9 @@
 package task_02;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import utilities.Driver;
@@ -14,7 +17,7 @@ public class Task2 {
     }
 
     @Test
-    public void task_2Test(){
+    public void task_2Test() {
 
         driver = Driver.getDriver();
 
@@ -23,18 +26,32 @@ public class Task2 {
 
 
         // 2. Click the JOIN NOW button to open the registration page
-
-
-
+        driver.findElement(By.xpath("//a[contains(text(),'Join Now!')]")).click();
 
 
         // 3. Select a title value from the dropdown
+        Select dropdown = new Select(driver.findElement(By.xpath("//select[@id='title']")));
+        dropdown.selectByValue("Mr");
+
+
         // 4. Enter your first name and surname in the form
-        // 5. Check the tick box with text 'I accept the Terms and Conditions and certify that I am over
-        //the age of 18.'
+        driver.findElement(By.xpath("//input[@id='forename']")).sendKeys("Edward");
+        driver.findElement(By.xpath("//input[@name='map(lastName)']")).sendKeys("Thorp");
+
+
+        // 5. Check the tick box with text 'I accept the Terms and Conditions and certify that I am over the age of 18.'
+        driver.findElement(By.xpath("(//input[@id='checkbox'])[3]")).click();
+
+
         // 6. Submit the form by clicking the JOIN NOW button
-        // 7. Validate that a validation message with text ‘ This field is required’
-        // appears under the date of birth box
+        driver.findElement(By.xpath("//input[@id='form']")).click();
+
+
+        // 7. Validate that a validation message with text ‘ This field is required’ appears under the date of birth box
+        String actualText = driver.findElement(By.xpath("//select[@id='dobYear']/following-sibling::label[1]")).getText();
+        String expectedText = "This field is required";
+
+        Assert.assertEquals(actualText, expectedText);
     }
 
 }
