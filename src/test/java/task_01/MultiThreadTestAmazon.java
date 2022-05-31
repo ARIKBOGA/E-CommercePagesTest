902456bb236cc69f87f8400847c305069ec5866f
burakarikboga <barikboga42@gmail.com>
2022-05-31 19:29:24 +0300

    Trying to execute multi test at the same time with Thread class / Runnable interface.

--- src/test/java/task_01/MultiThreadTestAmazon.java
+++ src/test/java/task_01/MultiThreadTestAmazon.java
@@ -1,18 +1,23 @@
 package task_01;
 
-public class MultiThreadTestAmazon implements Runnable {
+public class MultiThreadTestAmazon extends Thread {
     @Override
     public void run() {
         AmazonTest.amazonTest();
     }
+}
 
+class Main {
     public static void main(String[] args) {
         MultiThreadTestAmazon t1 = new MultiThreadTestAmazon();
         MultiThreadTestAmazon t2 = new MultiThreadTestAmazon();
-        //MultiThreadTestAmazon t3 = new MultiThreadTestAmazon();
-        //MultiThreadTestAmazon t4 = new MultiThreadTestAmazon();
-        //MultiThreadTestAmazon t5 = new MultiThreadTestAmazon();
-        t1.run();
-        t2.run();
+        MultiThreadTestAmazon t3 = new MultiThreadTestAmazon();
+        MultiThreadTestAmazon t4 = new MultiThreadTestAmazon();
+        MultiThreadTestAmazon t5 = new MultiThreadTestAmazon();
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
     }
 }
